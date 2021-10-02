8df13f3d2f9346bcc63602bf79737b4f38337e61
Awais <m.awais2030@gmail.com>
2021-10-02 14:34:41 +0500

    modified database

--- StudentManagementApp/models.py
+++ StudentManagementApp/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField
 
+# Creating class custom user and passing parent
+# AbstractUser so we can Extend the Default Auth User
+
+
+class CustomUser(AbstractUser):
+    user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
+    user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
+
+
 # Create your models here.
+
 
 class AdminHOD(models.Model):
     ID = models.AutoField(primary_key=True)
@@ -13,6 +24,8 @@ class AdminHOD(models.Model):
     Created_at = models.DateField(auto_now_add=True)
     Updated_at = models.DateField(auto_now_add=True)
     objects = models.Manager()
+    # Here making oneToOneField relation Between User Model and HOD model
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
 
 
 class Staffs(models.Model):
@@ -24,6 +37,9 @@ class Staffs(models.Model):
     Created_at = models.DateField(auto_now_add=True)
     Updated_at = models.DateField(auto_now_add=True)
     objects = models.Manager()
+    # Here making oneToOneField relation Between User Model and HOD model
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
 
 class Courses(models.Model):
     ID = models.AutoField(primary_key=True)
@@ -31,6 +47,7 @@ class Courses(models.Model):
     Created_at = models.DateField(auto_now_add=True)
     Updated_at = models.DateField(auto_now_add=True)
     objects = models.Manager()
+
 
 class Subject(models.Model):
     ID = models.AutoField(primary_key=True)
@@ -40,6 +57,7 @@ class Subject(models.Model):
     Created_at = models.DateField(auto_now_add=True)
     Updated_at = models.DateField(auto_now_add=True)
     objects = models.Manager()
+
 
 class Student(models.Model):
     ID = models.AutoField(primary_key=True)
@@ -52,8 +70,11 @@ class Student(models.Model):
     courseID = models.ForeignKey(Courses, on_delete=models.CASCADE)
     Created_at = models.DateField(auto_now_add=True)
     Updated_at = models.DateField(auto_now_add=True)
+    session_start_year = models.DateField()
+    session_end_year = models.DateField()
     objects = models.Manager()
-
+    # Here making oneToOneField relation Between User Model and HOD model
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
 
 class Attandance(models.Model):
     ID = models.AutoField(primary_key=True)
